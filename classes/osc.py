361da496849668d4e115d52aa11f26fe52733033
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2025-01-29 17:09:41 -0800

    Remove redundant methods and comments from VRChatOSC class for improved clarity

--- classes/osc.py
+++ classes/osc.py
@@ -1,7 +1,3 @@
-from pythonosc import udp_client
-
-
-class VRChatOSC:
 """
 Module for handling OSC (Open Sound Control) communications with VRChat.
 This module provides a class for sending messages and managing typing
@@ -30,16 +26,6 @@ class VRChatOSC:
 
         self.client = udp_client.SimpleUDPClient(ip, port)
 
-    def send_message(self, message: str):
-        """
-        Sends a message to the chatbox.
-        Args:
-            message (str): The message to be sent to the chatbox.
-        """
-
-        self.client.send_message("/chatbox/input", [message, True])
-        self.client.send_message("/chatbox/typing", False)
-
     def set_typing_indicator(self, typing: bool):
         """
         Sets the typing indicator status in the chatbox.
@@ -49,15 +35,6 @@ class VRChatOSC:
         """
 
         self.client.send_message("/chatbox/typing", typing)
-    def __init__(self, ip: str, port: int):
-        """
-        Initializes the OSC client with the specified IP address and port.
-        Args:
-            ip (str): The IP address of the OSC server.
-            port (int): The port number of the OSC server.
-        """
-
-        self.client = udp_client.SimpleUDPClient(ip, port)
 
     def send_message(self, message: str):
         """
@@ -68,13 +45,3 @@ class VRChatOSC:
 
         self.client.send_message("/chatbox/input", [message, True])
         self.client.send_message("/chatbox/typing", False)
-
-    def set_typing_indicator(self, typing: bool):
-        """
-        Sets the typing indicator status in the chatbox.
-        Args:
-            typing (bool): If True, the typing indicator will be shown. If
-            False, it will be hidden.
-        """
-
-        self.client.send_message("/chatbox/typing", typing)
