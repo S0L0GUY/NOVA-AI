e7e0330eae9431c30a9152c7437d7768719b0294
Evan Grinnell <worldevan8@gmail.com>
2025-11-10 12:57:34 -0800

    Update Adapter Logic (#83)
    * Refactor initialization logic into adapters for better modularity and error handling
    
    * Update SpeechRecognitionConfig to use GenAI model and adjust VAD aggressiveness
    
    * Condense logging configuration setup in main.py for improved readability
    
    * Sorted Imports
    
    * Sorted Imports (again)

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import logging
-import warnings
 
-import constants as constant
+import classes.adapters as adapters
 import nova
-from classes.osc import VRChatOSC
-from classes.vision_manager import VisionManager
-from classes.vrchat_api import VRChatAPIManager
 
-# Suppresses FP16 warnings on CPU
-warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
-
-# Configure logging to log errors to a file
+# Set up logging configuration
 logging.basicConfig(level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 def main() -> None:
     print("\033[91mProgram Starting...\033[0m")
 
-    # Initialize VRChat OSC object
-    osc = VRChatOSC(constant.Network.LOCAL_IP, constant.Network.VRC_PORT)
-
-    osc.send_message("System Starting")
-    osc.set_typing_indicator(True)
-
-    VisionManager.clear_vision_history()
-    VRChatAPIManager.create_vrchat_api_manager()
+    adapters.initialize_osc()
+    adapters.create_vrchat_api_manager()
 
     nova.main()
 
